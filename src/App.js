e1241123fce4408c6934a265e76eec62305ce6d4
yushun9096 <335607@gmail.com>
2023-04-22 15:55:33 +0800

    hope

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomButton from './co/CustomButton';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomButton a={'wordd'}/>
+      <CustomButton a={'wordd'}/>
+      <CustomButton a={"why"}/>
+      <CustomButton a={"why"}/>
+      <CustomButton a={'wordd'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/co/CustomButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomButton = ({a}) => {
+  return (
+    <button className='B'>
+        {a}
+    </button>
+  )
+}
+
+export default CustomButton
